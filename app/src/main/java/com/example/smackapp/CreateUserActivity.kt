a0589b9963daac2c2e9552a968e1fc879a842341
Brian Surface <iosmobilebrian@gmail.com>
2019-12-29 18:06:38 -0500

    set up avatar and background color generation

--- app/src/main/java/com/example/smackapp/CreateUserActivity.kt
+++ app/src/main/java/com/example/smackapp/CreateUserActivity.kt
@@ -1,10 +1,17 @@
 package com.example.smackapp
 
+import android.content.res.Resources
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profiledefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +19,23 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createAvatarClicked(view: View){
+
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+
+
+        if (color == 0) {
+
+            userAvatar = "light$avatar"
+        }else{
+
+            userAvatar = "dark$avatar"
+        }
+
+
+        val resourceId = resources.getIdentifier(userAvatar,"drawable",packageName)
+        createAvatarImageBtn.setImageResource(resourceId)
 
 
     }
@@ -22,6 +46,22 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateColorBtnClicked(view: View){
+
+        val random = Random()
+
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageBtn.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() /255
+        val savedG =g.toDouble() /255
+        val savedB = b.toDouble() /255
+
+        avatarColor = "[$savedR,$savedG,$savedB, 1]"
+
+        println(avatarColor)
 
 
     }
