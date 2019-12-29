13c541eb0125725ee669957e21a8b88169a7530f
Brian Surface <iosmobilebrian@gmail.com>
2019-12-29 13:31:33 -0500

    Create User Activity UI

---
+++ app/src/main/java/com/example/smackapp/CreateUserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.smackapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createAvatarClicked(view: View){
+
+
+    }
+
+    fun createUserBtnClicked(view: View){
+
+
+    }
+
+    fun generateColorBtnClicked(view: View){
+
+
+    }
+}
