d22c40960aa92a77733e0dca9d80750f208f36aa
Brian Surface <iosmobilebrian@gmail.com>
2020-05-12 21:56:26 -0400

    Broadcast receiver and clean-up UI after logout

--- app/src/main/java/com/example/smackapp/Services/UserDataService.kt
+++ app/src/main/java/com/example/smackapp/Services/UserDataService.kt
@@ -1,5 +1,8 @@
 package com.example.smackapp.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
 
     var id = ""
@@ -7,4 +10,50 @@ object UserDataService {
     var email = ""
     var avatarColor = ""
     var avatarName = ""
+
+    fun logout(){
+
+        id = ""
+        name = ""
+        email = ""
+        avatarColor = ""
+        avatarName = ""
+
+        AuthService.authToken = ""
+        AuthService.userEmail = ""
+        AuthService.isLoggedIn = false
+
+
+    }
+
+    fun returnAvatarColor(Components: String): Int{
+
+        val strippedColor = Components
+
+            .replace("[", "")
+            .replace("]","")
+            .replace(",","")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+
+
+        if (scanner.hasNext()){
+
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+
+
+
+        }
+
+
+        return  Color.rgb(r.g.b)
+
+
+    }
 }
